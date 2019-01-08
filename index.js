acf8e9177017491071b2ad9648377518fe54ae04
Victor Bruce <victorbruce82@gmail.com>
2019-01-08 17:56:14 +0000

    Test followup deployment with heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send({message: "Hello World!"});
 });
 
 const PORT = process.env.PORT || 3000;
