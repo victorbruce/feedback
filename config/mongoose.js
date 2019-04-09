a7565ea96e80b45ffe84f3bfe3e81072a36dcf77
Victor Bruce <victorbruce82@gmail.com>
2019-04-09 07:40:02 +0000

    Moved db connection to root file

--- config/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/online_payments', { useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(() => console.error('Failed to connect to MongoDB'));
-
-module.exports = {mongoose};
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const mongoose = require('./config/mongoose');
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+
+mongoose.connect(config.get('db'), { useNewUrlParser: true })
+    .then(() => console.log(`Connected to ${config.get('db')}`))
+    .catch(() => console.error('Failed to connect to MongoDB'));
 
 app.get("/", (req, res) => {
   res.send({message: "Hello World!"});
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log(`Server is running on port ${PORT}`));
